2568b7458e111bc8d6942fa6741781577f25d698
AM Smith <smitham42@gmail.com>
2023-04-13 14:51:12 -0700

    feat: remove template references, first dev release
    Signed-off-by: A.M. Smith <smitham42@gmail.com>

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,27 +1,7 @@
 #![doc = include_str!("../README.md")]
 
-use clap::Parser;
-
 pub mod config;
 pub mod grpc;
-
-// --------------------------------------------------
-// START REST SECTION
-// This section should be removed if there is no REST interface
-// --------------------------------------------------
-/// rest implementation module
-pub mod rest;
-
-/// struct holding cli configuration options
-#[derive(Parser, Debug)]
-pub struct Cli {
-    /// Target file to write the OpenAPI Spec
-    #[arg(long)]
-    pub openapi: Option<String>,
-}
-// --------------------------------------------------
-// END REST SECTION
-// --------------------------------------------------
 
 /// Tokio signal handler that will wait for a user to press CTRL+C.
 /// This signal handler can be used in our [`axum::Server`] method `with_graceful_shutdown`
@@ -29,20 +9,9 @@ pub struct Cli {
 ///
 /// # Examples
 ///
-/// ## axum
-/// ```
-/// use svc_template_rust::shutdown_signal;
-/// pub async fn server() {
-///     let app = axum::Router::new();
-///     axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
-///         .serve(app.into_make_service())
-///         .with_graceful_shutdown(shutdown_signal("rest"));
-/// }
-/// ```
-///
 /// ## tonic
 /// ```
-/// use svc_template_rust::shutdown_signal;
+/// use svc_contact::shutdown_signal;
 /// pub async fn server() {
 ///     let (_, health_service) = tonic_health::server::health_reporter();
 ///     tonic::transport::Server::builder()
@@ -55,5 +24,5 @@ pub async fn shutdown_signal(server: &str) {
     tokio::signal::ctrl_c()
         .await
         .expect("expect tokio signal ctrl-c");
-    rest_warn!("({}) shutdown signal", server);
+    grpc_warn!("({}) shutdown signal", server);
 }
