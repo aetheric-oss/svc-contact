dd4663a06018ba4d848150321acaa97f7091a8f9
AM Smith <smitham42@gmail.com>
2023-04-13 14:51:12 -0700

    feat: remove template references, first dev release

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,9 @@
 //! Main function starting the server and initializing dependencies.
 
-use clap::Parser;
 use dotenv::dotenv;
 use log::info;
-use svc_template_rust::config::Config;
-use svc_template_rust::grpc;
-use svc_template_rust::rest;
-use svc_template_rust::Cli;
+use svc_contact::config::Config;
+use svc_contact::grpc;
 
 /// Main entry point: starts gRPC Server on specified address and port
 #[tokio::main]
@@ -22,24 +19,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             panic!("(logger) could not parse {}. {}", log_cfg, e);
         }
     }
-
-    // --------------------------------------------------
-    // START REST SECTION
-    // This section should be removed if there is no REST interface
-    // --------------------------------------------------
-
-    // Allow option to only generate the spec file to a given location
-    // locally: cargo run -- --api ./out/$(PACKAGE_NAME)-openapi.json
-    // or `make rust-openapi` and `make rust-validate-openapi`
-    let args = Cli::parse();
-    if let Some(target) = args.openapi {
-        return rest::generate_openapi_spec(&target);
-    }
-
-    tokio::spawn(rest::server::rest_server(config.clone()));
-    // --------------------------------------------------
-    // END REST SECTION
-    // --------------------------------------------------
 
     let _ = tokio::spawn(grpc::server::grpc_server(config)).await;
 
