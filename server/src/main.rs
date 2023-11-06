048ccde558ebd8dbba766ddd05ab90270588f537
owlot <95369289+owlot@users.noreply.github.com>
2023-11-06 19:25:13 +0100

    chore: bring repo in line with template

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,39 @@
 //! Main function starting the server and initializing dependencies.
 
-use dotenv::dotenv;
 use log::info;
-use svc_contact::config::Config;
-use svc_contact::grpc;
+use svc_contact::*;
 
 /// Main entry point: starts gRPC Server on specified address and port
 #[tokio::main]
 #[cfg(not(tarpaulin_include))]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Will use default config settings if no environment vars are found.
-    let config = Config::from_env().unwrap_or_default();
+    let config = Config::try_from_env().unwrap_or_default();
 
-    dotenv().ok();
-    {
-        let log_cfg: &str = config.log_config.as_str();
-        if let Err(e) = log4rs::init_file(log_cfg, Default::default()) {
-            panic!("(logger) could not parse {}. {}", log_cfg, e);
-        }
+    // Try to load log configuration from the provided log file.
+    // Will default to stdout debug logging if the file can not be loaded.
+    load_logger_config_from_file(config.log_config.as_str())
+        .await
+        .or_else(|e| Ok::<(), String>(log::error!("(main) {}", e)))?;
+
+    info!("(main) Server startup.");
+
+    // Allow option to only generate the spec file to a given location
+    // locally: cargo run -- --api ./out/$(PACKAGE_NAME)-openapi.json
+    // or `make rust-openapi` and `make rust-validate-openapi`
+    let args = Cli::parse();
+    if let Some(target) = args.openapi {
+        return rest::generate_openapi_spec(&target);
     }
 
-    let _ = tokio::spawn(grpc::server::grpc_server(config)).await;
+    tokio::spawn(rest::server::rest_server(config.clone(), None));
 
-    info!("Server shutdown.");
+    tokio::spawn(grpc::server::grpc_server(config, None)).await?;
+
+    info!("(main) Server shutdown.");
+
+    // Make sure all log message are written/ displayed before shutdown
+    log::logger().flush();
+
     Ok(())
 }
