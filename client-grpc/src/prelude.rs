edd9f8a0f1156ab905c096b204e85aa33dba1ae2
A.M. Smith <smitham42@gmail.com>
2024-04-10 14:36:56 -0700

    feat: add cargo confirmation endpoint

--- client-grpc/src/prelude.rs
+++ client-grpc/src/prelude.rs
@@ -1,7 +1,7 @@
 //! Re-export of used objects
 
 pub use super::client as contact;
-pub use super::service::Client as TemplateRustServiceClient;
-pub use contact::TemplateRustClient;
+pub use super::service::Client as ContactServiceClient;
+pub use contact::ContactClient;
 
 pub use lib_common::grpc::Client;
